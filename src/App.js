a9dc1c0f6dabbc79a5b7584ab47fb0cf2ebea3c5
Prabin Kumar <prabink_22@apple.com>
2025-07-07 21:44:50 +0530

    build updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! This is a simple React application . It is deployed on AWS S3 using GitHub Actions.
+          My First React App on AWS S3 with GitHub Actions!
         </p>
         
       </header>
